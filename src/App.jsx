a64e1d72117ec8bae7c537c0698ac9c7310663ef
KasperPtak <kasperptak2@gmail.com>
2022-09-22 23:12:14 +0200

    Added search function for live games, getting player names on both teams
    Lots of fixes and additional data planned. First thing is error handling, and figuring out how to search for weird characters (æøåá...)

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,199 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import "./main.css";
 
 function App() {
+	const [searchText, setSearchText] = useState("");
+	const [playerData, setPlayerData] = useState({});
+	const [gameList, setGameList] = useState([]);
+	
+	const [liveGame, setLiveGame] = useState([]);
 
-  const [searchText, setSearchText] = useState('');
-  const [playerData, setPlayerData] = useState({});
-  const [gameList, setGameList] = useState([]);
-  
-  
-  const API_KEY = 'RGAPI-09cd130b-3fae-4738-9b9a-30cdea1c54c4';
+	const API_KEY = "RGAPI-3b8dd079-1867-4f46-9c2e-bc83d25db672";
 
-  function searchForPlayer(event) {
+	function searchForPlayer(event) {
+		// Set up correct api call
+		let APICallString =
+			"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
+			searchText +
+			"?api_key=" +
+			API_KEY;
 
-    // Set up correct api call 
-    let APICallString = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + searchText + '?api_key=' + API_KEY;
+		//handle api call
+		axios
+			.get(APICallString)
+			.then(function(response) {
+				setPlayerData(response.data);
+			})
+			.catch(function(error) {
+				console.log(error);
+				alert(error);
+			});
+	}
 
-    //handle api call
-    axios.get(APICallString).then(function (response) {
-    
-      setPlayerData(response.data);
+	function getPlayerGames(event) {
+		//handle api call til proxy server
+		axios
+			.get("http://localhost:4000/past5games", {
+				params: { username: searchText },
+			})
+			.then(function(response) {
+				setGameList(response.data);
+			})
+			.catch(function(error) {
+				console.log(error);
+				alert("error");
+			});
+	}
+	function getPlayerLiveGame(event) {
+		//handle api call til proxy server
+		
+		axios
+			.get("http://localhost:4000/liveGame", {
+				params: { username: searchText },
+			})
+			.then(function(response) {
+				setLiveGame(response.data);
+				
+			})
+			.catch(function(error) {
+				console.log(error);
+				alert("error");
+			});
+	}
 
-    }).catch(function (error) {
-      console.log(error)
-      alert(error)
-    })
+	// console.log(playerData)
+	// console.log(gameList);
+	console.log(liveGame);
 
-  }
+	function epochToJsDate(rd) {
+		// rd = revision date - epoch time
+		// returns date obj
 
-  function getPlayerGames(event) {
+		return new Date(rd);
+	}
 
-    //handle api call til proxy server
-    axios.get('http://localhost:4000/past5games', {params: { username: searchText }} )
-      .then(function (response) {
-        setGameList(response.data)
-      }).catch(function (error) {
-      console.log(error)
-      alert('error')
-    })
+	return (
+		<div className="App">
+			<div>
+				<h5>League Player Searcher</h5>
 
-  }
+				<input
+					type="text"
+					onKeyPress={(event) => {
+						if (event.key === "Enter") {
+							setSearchText(event.target.value);
+							searchForPlayer(event);
+							getPlayerGames();
+							
+						} else {
+							return;
+						}
+					}}
+					onChange={(event) => setSearchText(event.target.value)}
+				/>
 
-// console.log(playerData)
-console.log(gameList)
+				<button onClick={(event) => searchForPlayer(event)}>
+					Search for Player
+				</button>
+				<button onClick={(event) => getPlayerLiveGame(event)}>
+					Search for Players Live game
+				</button>
+				
+					
+					<div>
+					{liveGame.length !== 0 ? (
+				<>
+					<p>yes! data - gamelist</p>
+					<div className="container">
+						{liveGame.map((i ,index) => (
+							<>
+								<div>
+									<h2> player {index +1} : {i} </h2>
+									
+								</div>
+							</>
+						))}
+					</div>
+				</>
+			) : (
+				<>
+					<p>no! :C no gamelist data</p>
+				</>
+			)}
+					</div>
+				
+			</div>
 
-function epochToJsDate(rd){
-  // rd = revision date - epoch time
-  // returns date obj
-  
-  return new Date(rd);
-}
+			{JSON.stringify(playerData) !== "{}" ? (
+				<>
+					<p> {playerData.name} </p>
+					<img
+						width="100"
+						height="100"
+						src={
+							"http://ddragon.leagueoflegends.com/cdn/12.17.1/img/profileicon/" +
+							playerData.profileIconId +
+							".png"
+						}
+						alt="summoner "
+					/>
+					<p> Level : {playerData.summonerLevel}</p>
+					<p>
+						{" Sidste levelup : " +
+							epochToJsDate(playerData.revisionDate)}{" "}
+					</p>
 
-  return (
-    <div className="App">
-     <div className='container'>
+						
 
+					
 
-      <h5>League Player Searcher</h5>
+				</>
+			) : (
+				<>
+					<p>No Data :sadge: - please enter valid summonerName</p>
+				</>
 
-      <input type="text"
-      onKeyPress={(event) => {
-        if (event.key === "Enter") {
-          
-          setSearchText(event.target.value);
-          searchForPlayer(event);
-          getPlayerGames()
-        }
-        else {
-          return
-        }
-      }}
-      onChange={event => setSearchText(event.target.value) }  />
+				
 
-      <button onClick={event => searchForPlayer(event)}>Search for Player</button>
+			)}
 
-     </div>
+			
 
-    { JSON.stringify(playerData) !== '{}' ? 
-      <>
-      <p> { playerData.name } </p>
-      <img width='100' height='100' src={'http://ddragon.leagueoflegends.com/cdn/12.17.1/img/profileicon/' + playerData.profileIconId +'.png'} alt="summoner " />
-      <p> Level : { playerData.summonerLevel }</p>
-      <p>{ ' Sidste levelup : ' + epochToJsDate(playerData.revisionDate)}  </p>
-      
-     
-      </>
-      :
-      <><p>No Data :sadge:</p></>
-  }
-
-  { gameList.length !== 0 ?
-    <>
-      <p>yes! data</p>
-      {
-        gameList.map((gameData, index) =>
-          <>
-            <h2>Game {index + 1} </h2>
-            <div>
-              { gameData.info.participants.map((data, participantIndex) =>
-                <p>PLAYER { participantIndex + 1 } : { data.summonerName }, KDA: {data.kills} / {data.deaths} / {data.assists} </p>
-              ) }
-            </div>
-
-          </>
-        )
-      }
-    </>
-    :
-    <>
-    <p>no! :C no data</p>
-    </>
-  }
-
-
-
-    </div>
-  );
+			{gameList.length !== 0 ? (
+				<>
+					<p>yes! data - gamelist</p>
+					<div className="container">
+						{gameList.map((gameData, index) => (
+							<>
+								<div>
+									<h2>Game {index + 1} </h2>
+									<div className="scoreboard">
+										{gameData.info.participants.map(
+											(data, participantIndex) => (
+												<p>
+													PLAYER{" "}
+													{participantIndex + 1} :{" "}
+													{data.summonerName}, KDA:{" "}
+													{data.kills} / {data.deaths}{" "}
+													/ {data.assists}{" "}
+												</p>
+											)
+										)}
+									</div>
+								</div>
+							</>
+						))}
+					</div>
+				</>
+			) : (
+				<>
+					<p>no! :C no gamelist data</p>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
